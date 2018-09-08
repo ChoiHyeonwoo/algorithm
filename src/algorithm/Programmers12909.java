7c87d82a4bc7c6f38af568e0ebcff67816bc9f0f
ChoiHyeonWoo <201111267@sangmyung.kr>
2018-09-08 13:36:03 +0900

    올바른 괄호 문제 풀이 완료.

--- src/algorithm/Programmers12909.java
+++ src/algorithm/Programmers12909.java
@@ -1,10 +1,32 @@
 package algorithm;
 
 public class Programmers12909 {
+	public static boolean solution(String s) {
+        boolean answer = false;
+        int static_val = 0;
+        
+        for (int i =0 ; i< s.length(); i++) {
+        	if (s.charAt(i)=='(') {
+        		static_val++;
+        	} else {
+        		static_val--;
+        	}
+        	
+        	if (static_val < 0) {
+        		return false;
+        	}
+        }
+        
+        if (static_val == 0) {
+        	answer=true;
+        }
 
+        return answer;
+    }
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		System.out.println(solution("()()"));
+		System.out.println(solution(")()("));
 	}
 
 }
