762aa31ae4b57c1c18286e67204f3a44ba7fc58c
ChoiHyeonWoo <201111267@sangmyung.kr>
2018-09-08 14:14:28 +0900

    가장큰정사각형문제 building

---
+++ src/algorithm/Programmers12905.java
@@ -0,0 +1,18 @@
+package algorithm;
+
+public class Programmers12905 {
+	public static int solution(int [][]board)
+    {
+        int answer = 1234;
+
+        // [실행] 버튼을 누르면 출력 값을 볼 수 있습니다.
+        System.out.println("Hello Java");
+
+        return answer;
+    }
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
